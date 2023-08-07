efde22f34dbf5137f110abf5b5b9e006e485ff39
ichayer <ichayer@itba.edu.ar>
2023-08-07 20:09:24 -0300

    Update Cell model

--- src/ar/edu/itba/ss/cim/models/Cell.java
+++ src/ar/edu/itba/ss/cim/models/Cell.java
@@ -6,13 +6,11 @@ import java.util.List;
 public class Cell {
 
     private final List<Particle> particles;
-    private final Coordinates cellCoordinates;
-    private final double cellLarge;
+    private final double cellLength;
 
-    public Cell(Coordinates cellCoordinates, double cellLarge) {
+    public Cell(double cellLength) {
         this.particles = new ArrayList<>();
-        this.cellCoordinates = cellCoordinates;
-        this.cellLarge = cellLarge;
+        this.cellLength = cellLength;
     }
 
     public List<Particle> getParticles(){
@@ -21,6 +19,10 @@ public class Cell {
 
     public void addParticle(Particle particle){
         this.particles.add(particle);
+    }
+
+    public double getCellLength(){
+        return this.cellLength;
     }
 
 }
