b1886ae3c1e0baba50334ac635fadfd7e162547b
khcatino <kcatino@itba.edu.ar>
2023-08-07 12:50:38 -0300

    Initial commit with parsing of dynamic files and models

---
+++ src/ar/edu/itba/ss/cim/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.itba.ss.cim;
+
+import ar.edu.itba.ss.cim.utils.Fileparser;
+import ar.edu.itba.ss.cim.utils.IO;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       String STATIC_FILE_PATH = "static.txt";
+       String DYNAMIC_FILE_PATH = "dynamic.txt";
+//       String staticFile = IO.readFile(STATIC_FILE_PATH);
+       String dynamicFile = IO.readFile(DYNAMIC_FILE_PATH);
+       Map<Integer, List<Coordinates>> coordinatesMap = Fileparser.parseDynamicFile(dynamicFile);
+       System.out.println(coordinatesMap.get(1));
+
+    }
+}
---
+++ src/ar/edu/itba/ss/cim/models/Cell.java
@@ -0,0 +1,28 @@
+package ar.edu.itba.ss.cim.models;
+
+import ar.edu.itba.ss.cim.Coordinates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cell {
+
+    private final List<Particle> particles;
+    private Coordinates cellCoordinates;
+    private final double cellLarge;
+
+    public Cell(Coordinates cellCoordinates, double cellLarge) {
+        this.particles = new ArrayList<>();
+        this.cellCoordinates = cellCoordinates;
+        this.cellLarge = cellLarge;
+    }
+
+    public List<Particle> getParticles(){
+        return this.particles;
+    }
+
+    public void addParticle(Particle particle){
+        this.particles.add(particle);
+    }
+
+}
