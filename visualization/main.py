7417ee80e406dbda56a4c209b3d2bab7f2167f36
ichayer <ichayer@itba.edu.ar>
2023-08-09 01:18:59 -0300

    Start developing interactive plots

--- visualization/main.py
+++ visualization/main.py
@@ -1,7 +1,15 @@
+from visualization.models import Board, Particle
 from visualization.plots import plot_particles_in_board
-from visualization.utils import parse_json
 
-boards = parse_json()
 
-for board in boards:
-    plot_particles_in_board(board)
+# boards = parse_json()
+
+board = Board(20, 6, 1, 0,
+              [
+                  Particle(1, 2, 3, 1, [2, 3]),
+                  Particle(2, 15, 4, 3, []),
+                  Particle(3, 7, 5, 2, [2])
+              ]
+              )
+
+plot_particles_in_board(board)
--- visualization/plots.py
+++ visualization/plots.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 
@@ -5,22 +6,63 @@ from visualization.models import Board
 
 
 def plot_particles_in_board(board: Board):
-    fig, ax = plt.subplots(figsize=(8, 8))
+    # https://stackoverflow.com/questions/61583476/pycharm-not-displaying-matplotlib-plot
+    matplotlib.use('TkAgg')
 
+    fig, ax = plt.subplots()
+
+    circles_data = {}
     for particle in board.particles:
-        ax.add_patch(
-            Circle((particle.x, particle.y), radius=particle.radius, alpha=0.7)
-        )
+        circle = Circle((particle.x, particle.y), radius=particle.radius, alpha=0.7, color='b')
+        circles_data[particle.id] = {'particle': particle, 'circle': circle}
+        ax.add_patch(circle)
+
+    def motion_hover(event):
+        nonlocal circles_data
+        if event.inaxes == ax:
+            hovered_circle = None
+            hovered_particle = None
+            for particle_id, data in circles_data.items():
+                c = data['circle']
+                contains, _ = c.contains(event)
+                if contains:
+                    hovered_circle = c
+                    hovered_particle = data['particle']
+                    break
+            if hovered_circle and hovered_particle:
+                hovered_circle.set_color('red')
+                for neighbour_id in hovered_particle.neighbours:
+                    if neighbour_id in circles_data:
+                        neighbour_circle = circles_data[neighbour_id]['circle']
+                        neighbour_circle.set_color('green')
+            else:
+                for particle_id, data in circles_data.items():
+                    c = data['circle']
+                    original_color = 'b'  # Color original
+                    c.set_color(original_color)
+        fig.canvas.draw()
+
+    fig.canvas.mpl_connect('motion_notify_event', motion_hover)
+
+    subgrid_size = board.cell_length
+    x_ticks = []
+    y_ticks = []
+
+    for i in range(board.M + 1):
+        x_tick_value = subgrid_size * i
+        y_tick_value = subgrid_size * i
+        x_ticks.append(x_tick_value)
+        y_ticks.append(y_tick_value)
+
+    ax.set_xticks(x_ticks)
+    ax.set_yticks(y_ticks)
+    ax.grid()
 
     ax.set_xlim(0, board.L)
     ax.set_ylim(0, board.L)
+
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_title('Particle Distribution for time ' + str(board.time))
-    ax.grid()
 
     plt.show()
-
-
-
-
