3f887208bf64246e292827dfb8076dfa05270b9f
ichayer <ichayer@itba.edu.ar>
2023-08-09 15:25:58 -0300

    Improvements made on visualizations

--- visualization/plots.py
+++ visualization/plots.py
@@ -5,11 +5,8 @@ from matplotlib.patches import Circle
 from visualization.models import Board, TimeMeasures
 
 
-def plot_particles_in_board(board: Board):
-    # https://stackoverflow.com/questions/61583476/pycharm-not-displaying-matplotlib-plot
-    matplotlib.use('TkAgg')
-
-    fig, ax = plt.subplots()
+def configure_plot(board: Board):
+    fig, ax = plt.subplots(figsize=(6, 6))
 
     circles_data = {}
     for particle in board.particles:
@@ -17,33 +14,10 @@ def plot_particles_in_board(board: Board):
         circles_data[particle.id] = {'particle': particle, 'circle': circle}
         ax.add_patch(circle)
 
-    def motion_hover(event):
-        nonlocal circles_data
-        if event.inaxes == ax:
-            hovered_circle = None
-            hovered_particle = None
-            for particle_id, data in circles_data.items():
-                c = data['circle']
-                contains, _ = c.contains(event)
-                if contains:
-                    hovered_circle = c
-                    hovered_particle = data['particle']
-                    break
-            if hovered_circle and hovered_particle:
-                hovered_circle.set_color('red')
-                for neighbour_id in hovered_particle.neighbours:
-                    if neighbour_id in circles_data:
-                        neighbour_circle = circles_data[neighbour_id]['circle']
-                        neighbour_circle.set_color('green')
-            else:
-                for particle_id, data in circles_data.items():
-                    c = data['circle']
-                    original_color = 'b'  # Color original
-                    c.set_color(original_color)
-        fig.canvas.draw()
+    return fig, ax, circles_data
 
-    fig.canvas.mpl_connect('motion_notify_event', motion_hover)
 
+def configure_axes(board, ax):
     subgrid_size = board.cell_length
     x_ticks = []
     y_ticks = []
@@ -65,7 +39,42 @@ def plot_particles_in_board(board: Board):
     ax.set_ylabel('Y')
     ax.set_title('Particle Distribution for time ' + str(board.time))
 
-    plt.show()
+
+def motion_hover(event, plotting_data, ax, fig):
+    if event.inaxes == ax:
+        hovered_circle = None
+        hovered_particle = None
+        for particle_id, data in plotting_data.items():
+            c = data['circle']
+            contains, _ = c.contains(event)
+            if contains:
+                hovered_circle = c
+                hovered_particle = data['particle']
+                break
+        if hovered_circle and hovered_particle:
+            hovered_circle.set_color('red')
+            for neighbour_id in hovered_particle.neighbours:
+                if neighbour_id in plotting_data:
+                    neighbour_circle = plotting_data[neighbour_id]['circle']
+                    neighbour_circle.set_color('green')
+        else:
+            for particle_id, data in plotting_data.items():
+                c = data['circle']
+                original_color = 'b'
+                c.set_color(original_color)
+        fig.canvas.draw()
+
+
+def plot_particles_in_board(board):
+    matplotlib.use('TkAgg')
+
+    fig, ax, plotting_data = configure_plot(board)
+
+    fig.canvas.mpl_connect('motion_notify_event', lambda event: motion_hover(event, plotting_data, ax, fig))
+
+    configure_axes(board, ax)
+
+    return fig
 
 
 def plot_times(time_measures: TimeMeasures):
